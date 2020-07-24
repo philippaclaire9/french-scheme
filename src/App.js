bfb4031a00f23566e767c52fa1893ad30db6e239
Philippa <philippa.claire9@gmail.com>
2020-07-24 11:59:20 +0100

    start Y3Game1 component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Home from './Components/Home';
 import Nav from './Components/Nav';
 import Year3Start from './Components/Year3Start';
 import './App.css';
+import Y3Lesson1 from './Components/Y3Lesson1';
+import Y3Game1 from './Components/Y3Game1';
 function App() {
   return (
     <div className="App">
+      <h1>French appy app app</h1>
       <Nav />
       <Router>
         <Home path="/" />
         <Year3Start path="year_3" />
+        <Y3Lesson1 path="year_3/Lesson1" />
+        <Y3Game1 path="year_3/Lesson1/Game1" />
       </Router>
     </div>
   );
---
+++ src/Components/Y3Game1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Y3Game1 = () => {
+  return <div>Game 1</div>;
+};
+
+export default Y3Game1;
