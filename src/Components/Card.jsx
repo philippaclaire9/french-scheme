ccee0f7ad62438b19c5018a9ca7eed0e26cb6a0c
Philippa <philippa.claire9@gmail.com>
2020-07-28 12:01:26 +0100

    begin imperfect drag and drop logic

---
+++ src/Components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Card = (props) => {
+  const dragStart = (e) => {
+    const target = e.target;
+
+    e.dataTransfer.setData('card_id', target.id);
+
+    setTimeout(() => {
+      target.style.display = 'none';
+    }, 0);
+  };
+
+  const dragOver = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div
+      id={props.id}
+      onDragStart={dragStart}
+      draggable="true"
+      onDragOver={dragOver}
+      className={props.className}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
--- src/Components/Y3Game1.jsx
+++ src/Components/Y3Game1.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Draggable from 'react-draggable';
+import Boards from './Boards';
+import Card from './Card';
+import '../games.css';
 
-const Y3Game1 = () => {
-  return <div>Game 1</div>;
-};
+class Y3Game1 extends Component {
+  render() {
+    return (
+      <main className="flexbox">
+        <Boards id="board-main" className="start-board">
+          <Card id="bonjour-1" className="speaker-1">
+            Bonjour
+          </Card>
+          <Card id="bonjour-2" className="speaker-2">
+            Bonjour
+          </Card>
+        </Boards>
+        <br />
+        <br />
+        <Boards id="board-1" className="receiver-board">
+          1
+        </Boards>
+        <Boards id="board-2" className="receiver-board">
+          2
+        </Boards>
+
+        {/* <Draggable>
+          <div className="speaker-1">Bonjour</div>
+        </Draggable>
+        <Draggable>
+          <div className="speaker-2">Bonjour</div>
+        </Draggable>
+        <Draggable>
+          <div className="speaker-1">Je m'appelle Claude</div>
+        </Draggable>
+        <Draggable>
+          <div className="speaker-2">Je m'appelle Cléo</div>
+        </Draggable>
+        <div className="box"></div> */}
+      </main>
+    );
+  }
+}
 
 export default Y3Game1;
