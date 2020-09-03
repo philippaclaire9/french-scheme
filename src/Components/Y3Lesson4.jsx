d52077fa21e2e28fa00715d57f692352d92551b3
Philippa <philippa.claire9@gmail.com>
2020-09-03 17:10:58 +0100

    animate all three balloons

--- src/Components/Y3Lesson4.jsx
+++ src/Components/Y3Lesson4.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+//import '../animation.css';
+import '../animation-test.css';
+import balloonorange from '../images/balloons/balloonorange.jpg';
+import balloonpurple from '../images/balloons/balloonpurple.jpg';
+import balloonyellow from '../images/balloons/balloonyellow.jpg';
 
 const Y3Lesson4 = () => {
-  return <div>Allo allo</div>;
+  return (
+    <div className="centre-box">
+      <div className="balloon-container">
+        <img src={balloonorange} className="one" alt="orange balloon"></img>
+        <img src={balloonpurple} className="two" alt="purple balloon"></img>
+        <img src={balloonyellow} className="three" alt="yellow balloon"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Y3Lesson4;
